6cb9642bfe38dc3f04f773d37282fb66a8a282cd
Maria-Leiliane <maria.leiliane.lobo@gmail.com>
2025-09-13 23:29:25 -0300

    Adds Redis cache and completes the Search API CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableShutdownHooks();
+  const config = new DocumentBuilder()
+    .setTitle('Web Scop API')
+    .setDescription('API for asynchronous keyword monitoring on websites.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
--- src/search/service/search.service.ts
+++ src/search/service/search.service.ts
@@ -5,6 +5,7 @@ import { ISearchJobRepository } from '../store/search.store';
 import { DomainSearchJob, DomainSearchResult } from '../domain/search-job.entity';
 import { CrawlerService } from '../../crawler/crawler.service';
 import { SEARCH_JOB_REPOSITORY } from '../store/search.tokens';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 
 @Injectable()
 export class SearchService {
@@ -14,6 +15,8 @@ export class SearchService {
     @Inject(SEARCH_JOB_REPOSITORY)
     private readonly searchRepository: ISearchJobRepository,
     private readonly crawlerService: CrawlerService,
+    @Inject(CACHE_MANAGER)
+    private readonly cache: Cache,
   ) {}
 
   private generateId(): string {
@@ -29,29 +32,30 @@ export class SearchService {
       results: [],
       createdAt: new Date(),
     };
-
     await this.searchRepository.add(job);
-
     void this.startSearch(job, createSearchDto.url);
+
+    await this.cache.del('jobs:all');
+    this.logger.log('Cache de "jobs:all" invalid because the new creation.');
 
     return job;
   }
 
   async startSearch(job: DomainSearchJob, targetUrl: string): Promise<void> {
-    this.logger.log(`Starting background search for Job ID: ${job.id} na URL: ${targetUrl}`);
-
+    this.logger.log(`Init search in second plan for Job ID: ${job.id} na URL: ${targetUrl}`);
     try {
       const result = await this.crawlerService.crawlUrl(targetUrl, job.keyword);
       const foundResults = result ? [result] : [];
-      this.logger.log(`Search for Job ID: ${job.id} completed. ${foundResults.length} results found.`);
-
       await this.searchRepository.update(job.id, {
         results: foundResults,
         status: SearchStatus.DONE,
       });
 
+      await this.cache.del(`job:${job.id}`);
+      await this.cache.del('jobs:all');
+      this.logger.log(`Cache para Job ID: ${job.id} e 'jobs:all' invalid next search.`);
     } catch (error) {
-      this.logger.error(`Error in background search for Job ID: ${job.id}`, error);
+      this.logger.error(`Error in second plan search for Job ID: ${job.id}`, error);
       await this.searchRepository.update(job.id, {
         status: SearchStatus.DONE,
       });
@@ -59,24 +63,46 @@ export class SearchService {
   }
 
   async findById(id: string): Promise<DomainSearchJob> {
+    const cacheKey = `job:${id}`;
+
+    const cachedJob = await this.cache.get<DomainSearchJob>(cacheKey);
+    if (cachedJob) {
+      this.logger.log(`Job ID: ${id} not foud in CACHE.`);
+      return cachedJob;
+    }
+
+    this.logger.log(`Job ID: ${id} not foud in cache. Search in database...`);
     const job = await this.searchRepository.get(id);
     if (!job) {
-      throw new NotFoundException(`Search job with id ${id} not found`);
+      throw new NotFoundException(`Search Job with id ${id} not found`);
     }
+
+    await this.cache.set(cacheKey, job);
     return job;
   }
 
   async findAll(): Promise<DomainSearchJob[]> {
-    this.logger.log('Searching all search jobs...');
-    return this.searchRepository.getAll();
+    const cacheKey = 'jobs:all';
+
+    const cachedJobs = await this.cache.get<DomainSearchJob[]>(cacheKey);
+    if (cachedJobs) {
+      this.logger.log('List all jobs in CACHE.');
+      return cachedJobs;
+    }
+
+    this.logger.log('Job list not found in cache. Searching database...');
+    const jobs = await this.searchRepository.getAll();
+    await this.cache.set(cacheKey, jobs);
+    return jobs;
   }
+
   async remove(id: string): Promise<void> {
-    this.logger.log(`Trying to delete search job with ID: ${id}`);
-
     await this.findById(id);
-
     await this.searchRepository.remove(id);
 
-    this.logger.log(`Job de busca com ID: ${id} deletado com sucesso.`);
+    await this.cache.del(`job:${id}`);
+    await this.cache.del('jobs:all');
+
+    this.logger.log(`Job ID: ${id} and cache 'jobs:all' invalidated.`);
   }
 }
